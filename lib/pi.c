2d73292ecb603bab9a3cb344b5919f048025a262
olf@eisenzelt.de <olf@eisenzelt.de>
2026-05-27 06:13:24 +0200

    Remove ecc check, was always false

--- lib/pi.c
+++ lib/pi.c
@@ -56,16 +56,14 @@ uint8_t rds_pi_cc(uint16_t _pi)
 void rds_pi_decode(uint16_t _pi)
 {
     uint8_t cc_old;
-    uint8_t ecc_old;
 
     /* remember old CC */
     cc_old = rds_pi_cc(rds_program_current->pi);
-    ecc_old = rds_program_current->ecc;
 
     /* set new PI code */
     rds_program_current->pi = _pi;
 
     /* fill in ISO, ITU and CID, if necessary */
-    if ((cc_old != rds_pi_cc(_pi)) || (ecc_old != rds_program_current->ecc))
+    if (cc_old != rds_pi_cc(_pi))
         rds_ecc_get_iso_itu_cid();
 }
